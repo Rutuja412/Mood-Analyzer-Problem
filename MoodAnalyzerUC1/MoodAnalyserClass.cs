bf718126544d78482bb331c2f888e26bc8882bd8
Rutuja412 <rutujakadam4545@gmail.com>
2022-12-22 14:57:19 +0530

    [Ability to analyse mood]

---
+++ MoodAnalyzerUC1/MoodAnalyserClass.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MoodAnalyzerUC1
+{
+    internal class MoodAnalyserClass
+    {
+    }
+}
---
+++ MoodAnalyzerUC1/Program.cs
@@ -0,0 +1,10 @@
+﻿
+using System;
+using MoodAnalyzerUC1;
+
+
+Console.WriteLine("Sad or Happy");
+string input = Console.ReadLine();
+
+MoodAnalyserClass analyse = new MoodAnalyserClass(input);
+Console.WriteLine(analyse.Analyse());
