7524f690348ac48940416b02fad6213dfa66de71
Rutuja412 <rutujakadam4545@gmail.com>
2022-12-22 16:33:36 +0530

    [Message Reurn Sad]

---
+++ MoodAnalyzerTC1.1/Mood.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Text;
+
+namespace MoodAnalyzerTC1._1
+{
+    public class Mood
+    {
+        public string MoodAnalyser(string Mood)
+        {
+            if (Mood.ToLower().Contains("sad"))
+            {
+                Console.WriteLine("Sad");
+                return "Sad";
+            }
+
+            else
+            {
+                Console.WriteLine("Happy");
+                return "Happy";
+            }
+        }
+    }
+}
---
+++ MoodAnalyzerTC1.1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace MoodAnalyzerTC1._1
+{
+    public class Program
+    {
+        public static string Happy_Mood = "Happy Mood";
+        public static string Sad_Mood = "Sad Mood";
+        public static void Main(string[] args)
+        {
+            Mood m= new Mood();
+            m.MoodAnalyser(Happy_Mood);
+            m.MoodAnalyser(Sad_Mood);
+        }
+        
+          
+        
+    }
+}
